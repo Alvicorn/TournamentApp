a46ed680d91a642c72787db10dbf4c36151f3081
Alvin Tsang <alvincytsang@gmail.com>
2026-05-01 00:50:17 -0700

    minimize backend to only main:app for testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         # allow_origins=settings.cors_allow_origins,
-        allow_origins=True,
+        allow_origins=["http://localhost:5173"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
